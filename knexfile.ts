c57cb9f0349bc6b325aeec546dea58b14b8e37f3
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2026-03-12 17:01:53 -0300

    feat(database): add products table, Knex migrations and seed

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+import { after } from 'node:test';
+
+export default {
+  client: 'sqlite3',
+  connection: {
+    filename: './src/database/database.db',
+  },
+  pool: {
+    afterCreate: (conn: any, done: any) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
+    },
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extensions: '.ts',
+    directory: './src/database/migrations',
+  },
+  seeds: {
+    extensions: '.ts',
+    directory: './src/database/seeds',
+  },
+};
