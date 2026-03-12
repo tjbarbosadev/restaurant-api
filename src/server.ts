70208a48953fb5a1276b831dce686f44d5d0515e
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2026-03-12 15:36:58 -0300

    chore: initial project setup with Express, TypeScript and dotenv

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
